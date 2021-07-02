ee4a8c3fadb8fdba5774e8852a415c3afbd4610a
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-07-02 13:30:41 +0100

    Test: Working tests for API calls. Discover memory leak in image rerender, pending patch

--- src/spec/App.test.js
+++ src/spec/App.test.js
@@ -8,22 +8,22 @@ test('renders title', () => {
   expect(titleString).toBeInTheDocument();
 });
 
-// test('Can retrieve an extinct animal for taxonomy info', () => {
-//   render(<App />);
-//   const searchInput = screen.getByRole('textbox');
-//   userEvent.type(searchInput, "Entelodon");
-//   userEvent.click(screen.getByRole('button', {name: "=>"}));
-//   userEvent.click(screen.getByRole('button', {name: 'Taxonomy'}));
-//   const chordata = screen.getByText(/Phylum: Chordata/i);
-//   expect(chordata).toBeInTheDocument();
-// });
+test('Can retrieve an extinct animal for taxonomy info', async () => {
+  render(<App />);
+  const searchInput = screen.getByRole('textbox');
+  userEvent.type(searchInput, "Entelodon");
+  userEvent.click(screen.getByRole('button', {name: "=>"}));
+  userEvent.click(screen.getByRole('button', {name: 'Taxonomy'}));
+  const chordata = await screen.findByText(/Phylum: Chordata/i);
+  expect(chordata).toBeInTheDocument();
+});
 
-// test('Will display a thumbnail of the animal', () => {
-//   render(<App />);
-//   const searchInput = screen.getByRole('textbox');
-//   userEvent.type(searchInput, "Eretmorhipis");
-//   userEvent.click(screen.getByRole('button', {name: "=>"}));
-//   const eretmorhipis = screen.getByRole('img');
-//   expect(eretmorhipis).toHaveAttribute('src',
-//   'https://paleobiodb.org/data1.2/taxa/thumb.png?name=Eretmorhipis');
-// });
+test('Will display a thumbnail of the animal', async () => {
+  render(<App />);
+  const searchInput = screen.getByRole('textbox');
+  userEvent.type(searchInput, "Eretmorhipis");
+  userEvent.click(screen.getByRole('button', {name: "=>"}));
+  const eretmorhipis = await screen.findByRole('img');
+  expect(eretmorhipis).toHaveAttribute('src',
+  'https://paleobiodb.org/data1.2/taxa/thumb.png?name=Eretmorhipis');
+});
