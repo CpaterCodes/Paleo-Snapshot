d635e20354eb4e7eacce147f620571e7ae29c187
Charlie Paterson <Charlieed.paterson@googlemail.com>
2021-07-11 18:04:44 +0100

    Refactor: Core App component is now functional rather than class based

--- src/App.js
+++ src/App.js
@@ -1,62 +1,49 @@
 import './App.scss';
-import {Component} from 'react';
+import React, { useState } from 'react';
 import ImagePort from './components/ImagePort';
 import Sheet from './components/Sheet';
 import Search from './components/Search';
 import NullData from './components/assets/null_data.json';
 
-const emptyData = {
-  range: "Awaiting search",
-  ecology: "Awaiting search",
-  taxonomy: "Awaiting search"
-};
+export default function App(){
 
-class App extends Component {
+  const [data, setData] = useState(NullData);
+  const [img, setImg] = useState(null);
 
-  constructor(props){
-    super(props);
-    this.state = {dataSet: emptyData, img: null};
-    this.searchSubmit = this.searchSubmit.bind(this);
+  const searchSubmit = async(searchTerm) => {
+    await handleNewData(searchTerm);
+    await handleNewImage(searchTerm);
   }
 
-  async searchSubmit(searchTerm) {
-   await this.handleNewData(searchTerm);
-   await this.handleNewImage(searchTerm);
-  }
-
-  async handleNewData(searchTerm){
+  const handleNewData = async(searchTerm) => {
     const res = await fetch(`https://paleobiodb.org/data1.2/taxa/list.json?name=${searchTerm}&show=full`);
     if(!res.ok){
-      this.setState({dataSet: NullData});
+      setData(NullData);
       return
     }
     const data = await res.json();
-    this.setState({dataSet: data.records[0] ? data.records[0] : NullData});
+    setData(data.records[0] ? data.records[0] : NullData);
   }
 
-  async handleNewImage(searchTerm){
+  const handleNewImage = async(searchTerm) => {
     if(!searchTerm){
-      this.setState({img: null});
+      setImg(null);
       return
     }
-    const img = await fetch(`https://paleobiodb.org/data1.2/taxa/thumb.png?name=${searchTerm}`);
-    this.setState({img: img.status === 200 ? img.url : null});
+    const imgRes = await fetch(`https://paleobiodb.org/data1.2/taxa/thumb.png?name=${searchTerm}`);
+    setImg(imgRes.status === 200 ? imgRes.url : null);
   }
 
-  render() {
-    return (
-      <div className="App-body">
-        <header className="App-banner">
-          Paleo Snapshot
-        </header>
-        <main className="App-main">
-          <ImagePort img={this.state.img}/>
-          <Search onSubmit={this.searchSubmit}/>
-          <Sheet dataSet={this.state.dataSet}/>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div className="App-body">
+      <header className="App-banner">
+        Paleo Snapshot
+      </header>
+      <main className="App-main">
+        <ImagePort img={img}/>
+        <Search onSubmit={searchSubmit}/>
+        <Sheet dataSet={data}/>
+      </main>
+    </div>
+  );
 }
-
-export default App;
